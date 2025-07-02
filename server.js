b15bc979f2be01aabd42a78a1da1b776aad73ebb
Aarchi Mishra <aarchimishra0@gmail.com>
2025-07-02 23:47:31 +0530

    versal configuration added

--- server.js
+++ server.js
@@ -3,10 +3,20 @@ import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import incomeExpense from './router/IncomeExpenseRoutes.js'
 import userRoute from './router/UsersRoute.js';
+import cors from "cors";
 
 dotenv.config();
 const app = express();
+
+const corsOptions = {
+  origin: process.env.ORIGIN,
+  methods: ["POST", "PUT", "GET", "DELETE", "OPTIONS", "HEAD"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
+
 const port = process.env.PORT;
 
 app.get('/', (req, res) =>
